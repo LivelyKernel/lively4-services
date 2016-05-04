65192342efdd967aa130d33ef9197fef4154ebf8
Philipp Otto <philipp@scm.io>
2016-05-04 13:04:40 +0200

    add first tests

--- index.js
+++ index.js
@@ -18,85 +18,100 @@ function plainResponse(res, msg) {
   res.end(msg);
 }
 
-http.createServer(function(req, res) {
-  // Set CORS headers
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Request-Method', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET');
-  res.setHeader('Access-Control-Allow-Headers', '*');
+function start(cb) {
+  http.createServer(function(req, res) {
+    // Set CORS headers
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Request-Method', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET');
+    res.setHeader('Access-Control-Allow-Headers', '*');
 
-  if (req.method == "GET") {
-    if (req.url === "/start") {
-      ServiceManager.createScript("testScript.js", "setInterval(function() {console.log('test'); }, 1000 );");
-      ServiceManager.spawnProcess("testScript.js");
-      ServiceManager.startDebugServer();
-      return plainResponse(res, 'started');
-    } else if (req.url === "/stop") {
-      ServiceManager.shutdownDebugServer();
-      return plainResponse(res, 'stopped');
-    } else if (req.url === "/list") {
-      return jsonResponse(res, ServiceManager.listProcesses());
-    } else if (req.url.indexOf("/get") === 0) {
-      var url_parts = url.parse(req.url, true);
-      var query = url_parts.query;
-      if ('pid' in query) {
-        return jsonResponse(res, ServiceManager.getProcessInfo(query.pid));
+    if (req.method == "GET") {
+      if (req.url === "/start") {
+        ServiceManager.createScript("testScript.js", "setInterval(function() {console.log('test'); }, 1000 );");
+        ServiceManager.spawnProcess("testScript.js");
+        ServiceManager.startDebugServer();
+        return plainResponse(res, 'started');
+      } else if (req.url === "/stop") {
+        ServiceManager.shutdownDebugServer();
+        return plainResponse(res, 'stopped');
+      } else if (req.url === "/list") {
+        return jsonResponse(res, ServiceManager.listProcesses());
+      } else if (req.url.indexOf("/get") === 0) {
+        var url_parts = url.parse(req.url, true);
+        var query = url_parts.query;
+        if ('pid' in query) {
+          return jsonResponse(res, ServiceManager.getProcessInfo(query.pid));
+        }
+      } else {
+        return plainResponse(res, 'failed get');
+      }
+    } else if (req.method == "POST") {
+      var body = [];
+      if (req.url === "/start") {
+        req.on('data', function(chunk) {
+          body.push(chunk);
+        }).on('end', function() {
+          body = Buffer.concat(body).toString();
+          try {
+            var service = JSON.parse(body);
+            ServiceManager.createScript(service.name + ".js", service.code);
+            ServiceManager.spawnProcess(service.name + ".js");
+            return plainResponse(res, service.name + ' started');
+          } catch (ex) {
+            console.error(ex);
+          }
+        });
+      } else if (req.url === "/stop") {
+        req.on('data', function(chunk) {
+          body.push(chunk);
+        }).on('end', function() {
+          body = Buffer.concat(body).toString();
+          try {
+            var service = JSON.parse(body);
+            ServiceManager.killProcess(service.pid);
+            return plainResponse(res, 'killed');
+          } catch (ex) {
+            console.error(ex);
+          }
+        });
+      } else if (req.url === "/remove") {
+        req.on('data', function(chunk) {
+          body.push(chunk);
+        }).on('end', function() {
+          body = Buffer.concat(body).toString();
+          try {
+            var service = JSON.parse(body);
+            ServiceManager.removeProcess(service.pid);
+            return plainResponse(res, 'removed');
+          } catch (ex) {
+            console.error(ex);
+          }
+        });
+      } else {
+        return plainResponse(res, 'failed post');
       }
     } else {
-      return plainResponse(res, 'failed get');
+      return plainResponse(res, 'failed');
     }
-  } else if (req.method == "POST") {
-    var body = [];
-    if (req.url === "/start") {
-      req.on('data', function(chunk) {
-        body.push(chunk);
-      }).on('end', function() {
-        body = Buffer.concat(body).toString();
-        try {
-          var service = JSON.parse(body);
-          ServiceManager.createScript(service.name + ".js", service.code);
-          ServiceManager.spawnProcess(service.name + ".js");
-          return plainResponse(res, service.name + ' started');
-        } catch (ex) {
-          console.error(ex);
-        }
-      });
-    } else if (req.url === "/stop") {
-      req.on('data', function(chunk) {
-        body.push(chunk);
-      }).on('end', function() {
-        body = Buffer.concat(body).toString();
-        try {
-          var service = JSON.parse(body);
-          ServiceManager.killProcess(service.pid);
-          return plainResponse(res, 'killed');
-        } catch (ex) {
-          console.error(ex);
-        }
-      });
-    } else if (req.url === "/remove") {
-      req.on('data', function(chunk) {
-        body.push(chunk);
-      }).on('end', function() {
-        body = Buffer.concat(body).toString();
-        try {
-          var service = JSON.parse(body);
-          ServiceManager.removeProcess(service.pid);
-          return plainResponse(res, 'removed');
-        } catch (ex) {
-          console.error(ex);
-        }
-      });
-    } else {
-      return plainResponse(res, 'failed post');
-    }
-  } else {
-    return plainResponse(res, 'failed');
-  }
 
-}).listen(port, function(err) {
-  if (err) {
-    throw err;
-  }
-  console.log("Server running on port " + port);
-});
+  }).listen(port, function(err) {
+    if (err) {
+      throw err;
+    }
+    console.log("Server running on port " + port);
+    if (cb) {
+      cb();
+    }
+  });
+}
+
+
+if (!module.parent) {
+  // the script was directly executed
+  start();
+}
+
+module.exports = {
+  start: start
+};
---
+++ spec/test_spec.js
@@ -0,0 +1,77 @@
+var server = require("../index");
+var request = require('request');
+
+var route = 'http://localhost:9007';
+
+describe("Server", function() {
+  beforeAll(function(done) {
+    server.start(done);
+  })
+
+  it("gets empty list", function(done) {
+    request.get(
+      route + '/list',
+      function (error, response, body) {
+        expect(body).toBe("{}");
+        done();
+      }
+    );
+  });
+
+  it("starts test script", function(done) {
+    request.get(
+      route + '/start',
+      function (error, response, body) {
+        expect(body).toBe("started");
+        done();
+      }
+    );
+  });
+
+  it("gets empty list", function(done) {
+    request.get(
+      route + '/list',
+      function (error, response, body) {
+        var json = JSON.parse(body);
+        var pids = Object.keys(json);
+        expect(pids.length).toBe(1);
+
+        var child = json[pids[0]];
+        expect(child.name).toBe("testScript.js");
+        expect(child.status).toBe(1);
+        expect(child.kill).toBe(-1);
+
+        done();
+      }
+    );
+  });
+
+  it("starts test script", function(done) {
+    function getRunningPid(cb) {
+      request.get(
+        route + '/list',
+        function (error, response, body) {
+          var json = JSON.parse(body);
+          var pids = Object.keys(json);
+          expect(pids.length).toBe(1);
+
+          cb(pids[0]);
+        }
+      );
+    };
+
+    function kill(pid) {
+      done();
+      request.post(
+        route + '/stop',
+        JSON.stringify({pid: pid}),
+        function (error, response, body) {
+          expect(body).toBe("killed");
+          done();
+        }
+      );
+    };
+
+    getRunningPid(kill);
+  });
+});
