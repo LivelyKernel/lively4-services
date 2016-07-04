f0dae65a839da37d09943877509ab0e5bad143ce
Fabio Niephaus <code@fniephaus.com>
2016-07-04 11:53:54 +0200

    Use http-proxy because it supports websockets
    This requires us to use `http` instead of
    `express`.

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 require('babel-polyfill');
-var bodyParser = require('body-parser');
 var childProcess = require('child_process');
-var express = require('express');
-var proxy = require('express-http-proxy');
+var http = require("http");
+var httpProxy = require('http-proxy');
 var spawn = childProcess.spawn;
 var ServiceManager = require('./service_manager');
-var app = express();
+var app = http.createServer(dispatch);
+var proxy = httpProxy.createProxyServer({ ws: true });
 var config = require('./config');
 var promisify = require('promisify-node');
 var fs = promisify('fs');
@@ -35,93 +35,139 @@ function startLivelyServerInBackground() {
               config.LIVELY_SERVER_PORT + '...');
 }
 
-function start(cb) {
-  app.get('/', function(req, res) {
-    res.json({
-      status: 'running'
+function dispatch(req, res) {
+  // Set CORS headers
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Request-Method', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'POST, GET, OPTIONS');
+
+  if (req.url.indexOf("/lively/") === 0) {
+    req.url = req.url.substr('/lively'.length, req.url.length);
+    proxy.web(req, res, {
+      target: 'http://localhost:' + config.LIVELY_SERVER_PORT
     });
-  });
+    return;
+  } else if (req.url.indexOf("/debug/") === 0) {
+    req.url = req.url.substr('/debug'.length, req.url.length);
+    console.log('Proxy debug', req.url);
+    proxy.web(req, res, {
+      target: 'http://localhost:' + config.NODE_INSPECTOR_WEB_PORT
+    });
+    return;
+  }
 
-  app.use('/lively', proxy('localhost:' + config.LIVELY_SERVER_PORT, {
-    forwardPath: function(req, res) {
-      return require('url').parse(req.url).path;
-    }
-  }));
-
-  // Parse JSON data in body
-  app.use(bodyParser.json());
-
-  // CORS middleware
-  app.use(function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Request-Method', '*');
-    res.header('Access-Control-Allow-Methods', '*');
-    res.header("Access-Control-Allow-Headers", "Content-Type");
-
-    if ('OPTIONS' == req.method) {
-      res.sendStatus(200);
+  if (req.method === "GET") {
+    if (req.url === "/") {
+      return jsonResponse(res, { status: 'running' });
+    } else if (req.url === "/list") {
+      return jsonResponse(res, ServiceManager.listProcesses());
     } else {
-      next();
+      notFound(res);      
     }
-  });
+  } else if (req.method === "POST") {
+    console.log('POST', req.url);
+    var body = [];
+    req.on('data', function(chunk) {
+      body.push(chunk);
+    }).on('end', function() {
+      body = Buffer.concat(body).toString();
+      try {
+        var data = JSON.parse(body);
+        console.log(data);
 
-  app.get('/list', function(req, res) {
-    var list = ServiceManager.listProcesses();
-    return res.json(list);
-  });
-
-  app.post('/get', function(req, res) {
-    var service = req.body;
-    return ServiceManager.getProcessInfo(service.id).then(function(info) {
-      return res.json(info);
-    }).catch(function(error) {
-      res.json({ status: 'failed', message: error });
-    });
-  });
-
-  app.post('/start', function(req, res) {
-    var service = req.body;
-    var promise;
-    if (service.id !== undefined && ServiceManager.serviceExists(service.id)) {
-      ServiceManager.killProcess(service.id);
-      promise = Promise.resolve();
-    } else {
-      promise = ServiceManager.addService(service.entryPoint).then(function(id) {
-        service.id = id;
-      });
-    }
-    promise.then(function() {
-      ServiceManager.spawnProcess(service.id);
-      res.json({ status: 'success', pid: service.id });
-    }).catch(function(error) {
-      res.json({ status: 'failed', message: error });
-    });
-  });
-
-  app.post('/clone', function(req, res) {
-    var repoUrl = req.body.url;
-    var repoName = repoUrl.split("/").slice(-1)[0].split(".git")[0];
-    gitClone(repoUrl, config.servicesDir + "/" + repoName, function(error) {
-      if (error) {
-        res.json({status: 'failed', message: error});
-      } else {
-        res.json({ status: 'success'});
+        if (req.url === "/get") {
+          console.log('/GET');
+          postGet(req, res, data);
+        } else if (req.url === "/start") {
+          console.log('/start');
+          postStart(req, res, data);
+        } else if (req.url === "/clone") {
+          postClone(req, res, data);
+        } else if (req.url === "/stop") {
+          postStop(req, res, data);
+        } else if (req.url === "/remove") {
+          postRemove(req, res, data);
+        } else {
+          notFound(res);
+        }
+      } catch (ex) {
+        console.error(ex);
       }
+   });
+  } else if (req.method === "OPTIONS") {
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
+    res.writeHead(200);
+    res.end();
+  } else {
+    notFound(res);
+  }
+}
+
+function postGet(req, res, data) {
+  return ServiceManager.getProcessInfo(data.id).then(function(info) {
+    return jsonResponse(res, info);
+  }).catch(function(error) {
+    return jsonResponse(res, { status: 'failed', message: error });
+  });
+}
+
+function postStart(req, res, data) {
+  var promise;
+  if (data.id !== undefined && ServiceManager.serviceExists(data.id)) {
+    ServiceManager.killProcess(data.id);
+    promise = Promise.resolve();
+  } else {
+    promise = ServiceManager.addService(data.entryPoint).then(function(id) {
+      data.id = id;
     });
+  }
+  promise.then(function() {
+    ServiceManager.spawnProcess(data.id);
+    jsonResponse(res, { status: 'success', pid: data.id });
+  }).catch(function(error) {
+    jsonResponse(res, { status: 'failed', message: error });
   });
-
-  app.post('/stop', function(req, res) {
-    var service = req.body;
-    ServiceManager.killProcess(service.id);
-    return res.json({ status: 'success' });
+}
+function postClone(req, res, data) {
+  var repoUrl = data.url;
+  var repoName = repoUrl.split("/").slice(-1)[0].split(".git")[0];
+  gitClone(repoUrl, config.servicesDir + "/" + repoName, function(error) {
+    if (error) {
+      jsonResponse(res, { status: 'failed', message: error });
+    } else {
+      jsonResponse(res, { status: 'success'});
+    }
   });
+}
+function postStop(req, res, data) {
+  ServiceManager.killProcess(data.id);
+  return jsonResponse(res, { status: 'success' });
+}
+function postRemove(req, res, data) {
+  ServiceManager.removeProcess(data.id);
+  return jsonResponse(res, { status: 'success'});
+}
 
-  app.post('/remove', function(req, res) {
-    var service = req.body;
-    ServiceManager.removeProcess(service.id);
-    return res.json({ status: 'success'});
+function notFound(res) {
+  res.writeHead(404);
+  res.end();
+}
+
+function jsonResponse(res, obj) {
+  res.writeHead(200, {
+    'Content-Type': 'application/json'
   });
+  res.end(JSON.stringify(obj));
+}
 
+app.on('upgrade', function (req, socket, head) {
+  console.log("proxying upgrade request", req.url);
+  proxy.ws(req, socket, head, {
+      target: 'ws://localhost:' + config.NODE_INSPECTOR_WEB_PORT
+    });
+});
+
+function start(cb) {
   app.listen(config.PORT, function () {
     startLivelyServerInBackground();
     ServiceManager.startDebugServer();
@@ -131,7 +177,6 @@ function start(cb) {
     }
   });
 }
-
 
 if (!module.parent) {
   // the script was directly executed
