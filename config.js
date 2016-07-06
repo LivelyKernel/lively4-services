9d210b50254895295f63810179bfc0a0fd1fc440
Fabio Niephaus <code@fniephaus.com>
2016-07-06 20:34:27 +0200

    Allow heroku to set port

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  PORT: process.env.LK4_SERVICES_PORT || 9007,
+  PORT: process.env.PORT || 9007, // Allow Heroku to set port
   LIVELY_SERVER_PORT: 8901,
   LIVELY_SERVER_PATH: './node_modules/lively4-server/httpServer.js',
   NODE_INSPECTOR_WEB_PORT: 8902,
